77684a9249fce21cd3eb309a3491d6053a950c98
Luke Switzer <luke@rootdowndigital.com>
2022-07-07 09:35:52 -0600

    Initial Commit

---
+++ Shared/LukeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LukeCardApp.swift
+//  Shared
+//
+//  Created by Luke on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LukeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
